fd7ea10d38df885d698f9acf01b8398fde022298
omantel <oliviermantel@gmail.com>
2020-10-05 09:21:48 +0200

    Clean main js

--- js/main.js
+++ js/main.js
@@ -7,39 +7,53 @@ fetchPromise
         if (response.ok) {
             return response.json();
         } else {
-            throw error = response.status;
+            throw error;
         }
     })
     .then(objects => {
-        //Affiche dynamiquement la liste des produits
-        console.log(objects);
-        let elements = "";
-        let itemsElements = document.querySelector(".items");
-
-        objects.forEach(products => {
-            elements +=
-                `<section class="product__list">
-                    <a href="page-product.html?id=${products._id}">
-                    <div class="product__image-small">
-                        <img src="${products.imageUrl}" alt="">
-                    </div>
-                    <div class="product__infos"> 
-                        <div class="productName"  id="${products._id}">${products.name}</div>
-                        <div class="productPrice">Prix: ${(products.price / 100)} €</div>
-                    </div>
-                    </a>
-                </section>`;
-        });
-        loaderHide()
-        itemsElements.innerHTML = elements;
+        displayProducts(objects);
+        loaderHide();
     })
     .catch((error) => {
-        console.error(error);
         loaderHide()
-        itemsElements.innerHTML = error;
+        displayError(error)
     });
+
+
+
+function displayProducts(elt) {
+    // console.log(elt);
+    let elements = "";
+    let itemsElements = document.querySelector(".items");
+
+    elt.forEach(products => {
+        elements +=
+            `<section class="product__list">
+                <a href="page-product.html?id=${products._id}">
+                <div class="product__image-small">
+                    <img src="${products.imageUrl}" alt="">
+                </div>
+                <div class="product__infos"> 
+                    <div class="productName"  id="${products._id}">${products.name}</div>
+                    <div class="productPrice">Prix: ${(products.price / 100)} €</div>
+                </div>
+                </a>
+            </section>`;
+    });
+    itemsElements.innerHTML = elements;
+
+}
 
 function loaderHide() {
     let loader = document.querySelector(".loader");
     loader.classList.add("hidden");
+}
+
+function displayError(elt) {
+    let itemsElements = document.querySelector(".items");
+    console.error(elt)
+    let msg = "";
+    msg +=
+        `<p> Oops...un erreur<p>`
+    itemsElements.innerHTML = msg;
 }
