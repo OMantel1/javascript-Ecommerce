9c899e9bb6a9d3abcb5f066204f3a32f057fbe84
omantel <oliviermantel@gmail.com>
2020-10-05 14:23:18 +0200

    Clean confirmations pages

--- js/page-confirmation.js
+++ js/page-confirmation.js
@@ -1,33 +1,26 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-    //variables
     let commandData = JSON.parse(localStorage.getItem("command"));
     let basket = JSON.parse(localStorage.getItem("panier"));
     let commandMessage = document.getElementById("validate");
-
-
     let price = totalPrice(basket);
 
-    //Ajout du message de bonne reception de la commande
-    function addMessage() {
-
-        let message = "";
-        message = `
-            <h2>${commandData.contact.firstName}, merci d'être passé par Oridteddies!</h2>
-            <p>Votre commande sera traitée dans les meilleurs délais.</p>
-            <p>Réf. commande: ${commandData.orderId}</p>
-            <p>Montant: ${price} Euros</p>`;
-        commandMessage.innerHTML = message;
-        console.log(commandData);
-
-    }
-    addMessage();
-
+    addMessage(commandData, price, commandMessage);
     resetLocalStorage();
 });
 
 
-// calcul du prix total de la commande
+
+function addMessage(commandData, price, commandMessage) {
+    let message = "";
+    message = `
+            <h2>${commandData.contact.firstName}, merci d'être passé par Oridteddies!</h2>
+            <p>Votre commande sera traitée dans les meilleurs délais.</p>
+            <p>Réf. commande: ${commandData.orderId}</p>
+            <p>Montant: ${price} Euros</p>`;
+    commandMessage.innerHTML = message;
+}
+
 function totalPrice(arr) {
     let total = 0;
     arr.forEach(product => {
@@ -36,12 +29,13 @@ function totalPrice(arr) {
     return total;
 }
 
-// remise à zero du local storage
 function resetLocalStorage() {
     localStorage.removeItem("panier");
     localStorage.removeItem("IdClicked");
 }
 
 
-export {totalPrice, resetLocalStorage};
-
+export {
+    totalPrice,
+    resetLocalStorage
+};
