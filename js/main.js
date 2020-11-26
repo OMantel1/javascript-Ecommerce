99e2de7ab939f6d817cefdb211be3ce6a6567d18
omantel <oliviermantel@gmail.com>
2020-11-26 14:41:02 +0100

    Refactor main.js api call

--- js/main.js
+++ js/main.js
@@ -1,59 +1,70 @@
-const fetchPromise =
-    fetch("https://oriteddies-api.herokuapp.com/api/teddies");
+// const fetchPromise = fetch("https://oriteddies-api.herokuapp.com/api/teddies");
 
-fetchPromise
-    .then(response => {
-        console.log(response)
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw error;
-        }
-    })
-    .then(objects => {
-        displayProducts(objects);
-        loaderHide();
-    })
-    .catch((error) => {
-        loaderHide()
-        displayError(error)
-    });
+// fetchPromise
+//   .then((response) => {
+//     console.log(response);
+//     if (response.ok) {
+//       return response.json();
+//     } else {
+//       throw error;
+//     }
+//   })
+//   .then((response) => {
+//     displayProducts(response);
+//     loaderHide();
+//   })
+//   .catch((error) => {
+//     loaderHide();
+//     displayError(error);
+//   });
 
+async function apiCall() {
+  const api = await fetch("https://oriteddies-api.herokuapp.com/api/teddies");
+  const response = api.json();
+  return response;
+}
+
+apiCall()
+  .then((response) => {
+    displayProducts(response);
+    loaderHide();
+  })
+  .catch((error) => {
+    loaderHide();
+    displayError(error);
+  });
 
 
 function displayProducts(elt) {
-    // console.log(elt);
-    let elements = "";
-    let itemsElements = document.querySelector(".items");
+  let elements = "";
+  let itemsElements = document.querySelector(".items");
 
-    elt.forEach(products => {
-        elements +=
-            `<section class="product__list">
-                <a href="page-product.html?id=${products._id}">
-                <div class="product__image-small">
-                    <img src="${products.imageUrl}" alt="">
-                </div>
-                <div class="product__infos"> 
-                    <div class="productName"  id="${products._id}">${products.name}</div>
-                    <div class="productPrice">Prix: ${(products.price / 100)} €</div>
-                </div>
-                </a>
-            </section>`;
-    });
-    itemsElements.innerHTML = elements;
-
+  elt.forEach((products) => {
+    elements += 
+    `<section class="product__list">
+        <a href="page-product.html?id=${products._id}">
+        <div class="product__image-small">
+            <img src="${products.imageUrl}" alt="">
+        </div>
+        <div class="product__infos"> 
+            <div class="productName"  id="${products._id}">${products.name}</div>
+            <div class="productPrice">Prix: ${(products.price / 100)} €</div>
+        </div>
+        </a>
+    </section>`;
+  });
+  itemsElements.innerHTML = elements;
 }
 
 function loaderHide() {
-    let loader = document.querySelector(".loader");
-    loader.classList.add("hidden");
+  let loader = document.querySelector(".loader");
+  loader.classList.add("hidden");
 }
 
 function displayError(elt) {
-    let itemsElements = document.querySelector(".items");
-    console.error(elt)
-    let msg = "";
-    msg +=
-        `<p> Oops...un erreur<p>`
-    itemsElements.innerHTML = msg;
+  let itemsElements = document.querySelector(".items");
+  let msg = "";
+  msg += `<p> Oops...un erreur : <br><i>${elt}<i><p>`;
+  itemsElements.innerHTML = msg;
 }
+
